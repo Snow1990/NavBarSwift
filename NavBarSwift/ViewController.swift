effe67f20a6db118c8ec409f2441f19fbe33a885
Snow1990 <snn@shaonandeMacBook-Pro.local>
2015-06-08 09:33:15 +0800

    Initial Commit

---
+++ NavBarSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavBarSwift
+//
+//  Created by SN on 15/6/8.
+//  Copyright (c) 2015年 Snow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
